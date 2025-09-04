a25368fd4068ec4eefe6c1542ae889993808bbc9
Tamilalakan <43065774+tamilalakan@users.noreply.github.com>
2025-09-04 21:07:50 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/todo-app/', // replace with your repo name
-  plugins: [react()],
-})
